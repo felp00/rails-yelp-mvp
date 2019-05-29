5f48c0fd7151245721143fd33ff2dffd5fddbbed
Felipe Rocha <felipewp200@gmail.com>
2019-05-29 17:09:46 -0300

    restaurants

--- db/migrate/20190529152720_create_reviews.rb
+++ db/migrate/20190529152720_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.text :content
-      t.integer :rating, numericality: { only_integer: true }
+      t.integer :rating
       t.references :restaurant, foreign_key: true
 
       t.timestamps
